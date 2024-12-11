fee44a12eb4ae2f560d8deca8e11df5b2b2b4ba2
BOYOUNG-KANG <by28287@gmail.com>
2024-12-11 14:16:29 +0900

    first commit

---
+++ src/main/java/infra_pusija/infra_1week/Infra1weekApplication.java
@@ -0,0 +1,13 @@
+package infra_pusija.infra_1week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Infra1weekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Infra1weekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/infra_pusija/infra_1week/Infra1weekApplicationTests.java
@@ -0,0 +1,13 @@
+package infra_pusija.infra_1week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Infra1weekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
