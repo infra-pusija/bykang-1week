1639fb8fcce937bb681044712dccb7045949bacd
BOYOUNG-KANG <by28287@gmail.com>
2024-12-22 17:53:05 +0900

    fix : test 도메인 allargsconstructor 삭제 및 getter, setter 추가

--- src/main/java/infra_pusija/infra_1week/Test.java
+++ src/main/java/infra_pusija/infra_1week/Test.java
@@ -8,12 +8,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table
 @Data
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
 public class Test {
 
