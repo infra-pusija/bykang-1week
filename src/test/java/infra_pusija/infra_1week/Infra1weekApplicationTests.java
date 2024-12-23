0bbe61e1f6991763de81aa6e4cba40966e1bb3f7
BOYOUNG-KANG <by28287@gmail.com>
2024-12-23 22:37:56 +0900

    fix : application-test.properties 설정 추가

--- src/test/java/infra_pusija/infra_1week/Infra1weekApplicationTests.java
+++ src/test/java/infra_pusija/infra_1week/Infra1weekApplicationTests.java
@@ -2,8 +2,10 @@ package infra_pusija.infra_1week;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Infra1weekApplicationTests {
 
 	@Test
